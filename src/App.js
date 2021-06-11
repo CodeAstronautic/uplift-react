0400d2dd23af10eb822a171026d034df7dc0721d
priyanshuhaldar007 <52622977+priyanshuhaldar007@users.noreply.github.com>
2021-06-11 22:07:23 +0530

    original app.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,17 @@
-import './App.css';
-import React,{ useState } from 'react';
+import React from "react";
+import {add,sub, mul, div}  from './Calc'
 
 
-const App=()=> {
-
-  const [state, setState]=useState("");
-  
-
-  const sum=(e)=>{
-
-    const value=e.target.value ;
-
-    if(value==="="){
-      try{setState(eval(state));}
-      catch(err){
-      setState("math error");}
-
-    }else if(value==="C"){
-      setState("")
-    }else if(value==="Del"){
-      setState(state.substr(0,state.length-1));
-    }
-    else{
-    setState(state + value)
-    }
-  };
-  
-
-  return (
-    <div className="body">
-      <h1>Calculator</h1>
-      <div className="fields">
-        {state}
-        
-      </div>
-      <div className="buttons">
-        <div className="row-1">
-          <button onClick={sum} value="C">C</button>
-          <button onClick={sum} value="Del">Del</button>
-          <button onClick={sum} value="/">/</button>
-          <button onClick={sum} value="*">*</button>
-        </div>
-        <div className="row-2">
-          <button onClick={sum} value="7">7</button>
-          <button onClick={sum} value="8">8</button>
-          <button onClick={sum} value="9">9</button>
-          <button onClick={sum} value="-">-</button>
-        </div>
-        <div className="row-3">
-          <button onClick={sum} value="4">4</button>
-          <button onClick={sum} value="5">5</button>
-          <button onClick={sum} value="6">6</button>
-          <button onClick={sum} value="+">+</button>
-        </div>
-        <div className="row-4">
-          <button onClick={sum} value="1">1</button>
-          <button onClick={sum} value="2">2</button>
-          <button onClick={sum} value="3">3</button>
-          <button onClick={sum} value="%">%</button>
-        </div>
-        <div className="row-5">
-          <button id="zero" onClick={sum} value="0">0</button>
-          <button onClick={sum} value=".">.</button>
-          <button id="equal" onClick={sum} value="=">=</button>
-        </div>
-      </div>
-    
-    </div>
-  );
-  
+function App(){
+  return(
+    <>
+    <ul>
+      <li>The sum of two numbers is {add(40,4)}</li>
+      <li>The diff of two numbers is {sub(40,4)}</li>
+      <li>The multiplication of two numbers is {mul(40,4)}</li>
+      <li>The division of two numbers is {div(40,4)}</li>
+    </ul>
+    </>
+  )
 }
-
 export default App;
