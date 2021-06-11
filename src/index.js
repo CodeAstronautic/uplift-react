5957a7ef7a8438a40c700d38716531ff462374a0
devanshi mishra <devu2732@gmail.com>
2021-06-11 20:37:42 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [input, setInput] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => {
+          setInput(input + e.target.value);
+        }}
+        value={item}
+        key={item}
+      >
+        {" "}
+        {item}
+      </button>
+    );
+  });
+
+  return (
+    <div className="wrapper">
+      {" "}
+      <div className="show-input">{input}</div>
+      <div className="digits flex">{calcBtns}</div>
+      <div className="modifiers subgrid">
+       
+
+        <button onClick={() => setInput(input.substr(0, input.length - 1))}>
+          Clear
+        </button>
+
+ 
+        <button onClick={() => setInput("")} value="">
+          AC
+        </button>
+      </div>
+      <div className="operations subgrid">
+        
+        <button onClick={(e) => setInput(input + e.target.value)} value="+">
+          +
+        </button>
+
+       
+        <button onClick={(e) => setInput(input + e.target.value)} value="-">
+          {" "}
+          -{" "}
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="*">
+          {" "}
+          *
+        </button>
+
+        <button onClick={(e) => setInput(input + e.target.value)} value="/">
+          {" "}
+          /
+        </button>
+      
+        <button
+          onClick={(e) => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
