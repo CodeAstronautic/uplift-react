f4f47d8679783cc83197b12c838b39ac2907233a
pooja <mishrapooja8128@gmail.com>
2021-06-12 08:13:08 -0700

    added map & get api country

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
 import Demo from "./demo";
-
+import axios from "axios"
 const App = () => {
   const [customer, setCustomer] = useState(0);
   const [text, setText] = useState("uplift");
 
-  const handleIn = () => {
-    setCustomer(customer + 1);
-  };
-  const handleClick = () => {
-    setText("uplkift 1");
-  };
+  // const handleIn = () => {
+  //   setCustomer(customer + 10);
+  // };
+  // const handleClick = () => {
+  //   setText("uplkift 1");
+  // };
+
+  useEffect(() => {
+    axios
+      .get("https://api.first.org/data/v1/countries")
+      .then((res) => {
+        setText(res?.data?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(
+    Object.entries(text).map((D) => {
+      console.log(D[1].country);
+    })
+  );
   return (
-    <div>
-      <h1>{text}</h1>
-      <h1>{customer}</h1>
+    <div style={{ textAlign: "center" }}>
+      <table>
+        <tr>
+          <th>Country</th>
+        </tr>
+        {Object.entries(text).map((d) => {
+            return (
+              <tr>
+                <td>{d[1].country}</td>
+              </tr>
+            );
+          })}
+      </table>
+      {/* <h1>{customer}</h1>
       <button onClick={handleIn}> Increment</button>
       <button onClick={() => setCustomer(customer - 1)}> minus</button>
       <button onClick={handleClick}> Text 123</button>
