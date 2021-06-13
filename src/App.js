8a509e26f0ba0e469a9b842fad78ad814479398f
AmanBisht01 <bishtaman1111@gmail.com>
2021-06-13 18:26:22 +0530

    AmanBisht Calculator

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
-import React from "react";
-import {add,sub, mul, div}  from './Calc'
+import { React, useState } from "react";
+import "./App.css";
 
+function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
 
-function App(){
-  return(
-    <>
-    <ul>
-      <li>The sum of two numbers is {add(40,4)}</li>
-      <li>The diff of two numbers is {sub(40,4)}</li>
-      <li>The multiplication of two numbers is {mul(40,4)}</li>
-      <li>The division of two numbers is {div(40,4)}</li>
-    </ul>
-    </>
-  )
+  function add() {
+    setTotal(number1 + number2);
+  }
+  function subtract() {
+    setTotal(number1 - number2);
+  }
+  function Multiply() {
+    setTotal(number1 * number2);
+  }
+  function devide() {
+    setTotal(number1 / number2);
+  }
+
+  function Mod() {
+    setTotal(number1 % number2);
+  }
+  return (
+    <div className="App">
+      <h1>Calculator</h1>
+
+      <div className="number-inputs">
+        <input
+          type="number"
+          onChange={(e) => setNumber1(+e.target.value)}
+          placeholder="First Number"
+        />
+        <input
+          type="number"
+          onChange={(e) => setNumber2(+e.target.value)}
+          placeholder="Second Number"
+        />
+      </div>
+      <div className="btn">
+        <button className="btn-1" onClick={add}>
+          Addition
+        </button>
+        <button className="btn-2" onClick={subtract}>
+          Subtract
+        </button>
+        <button className="btn-3" onClick={Multiply}>
+          Multiply
+        </button>
+        <button className="btn-4" onClick={devide}>
+          Devide
+        </button>
+        <button className="btn-5" onClick={Mod}>
+          Modulo
+        </button>
+      </div>
+      <h2>{total}</h2>
+    </div>
+  );
 }
 export default App;
