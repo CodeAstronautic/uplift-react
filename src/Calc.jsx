d96b1f4d2f891fa72c950e66deff4ec92ee3fb3b
Somasree Majumder <bishnupadamajumder32@gmail.com>
2021-06-13 14:07:07 +0530

    Adding the files

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -1,18 +1,20 @@
-
-function add(a,b){
-    let sum =a +b;
+function add(a, b) {
+    let sum = a + b;
     return sum;
 }
-function sub(a,b){
-    let diff =a - b;
+
+function sub(a, b) {
+    let diff = a - b;
     return diff;
 }
-function mul(a,b){
-    let mul =a*b;
+
+function mul(a, b) {
+    let mul = a * b;
     return mul;
 }
-function div(a,b){
-    let div =a /b;
+
+function div(a, b) {
+    let div = a / b;
     return div;
 }
-export {add,sub, mul, div};
+export { add, sub, mul, div };
