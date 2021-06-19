31c0923f8973f4db899033d869252dd226c4c627
Ashutosh <vu4f1920027@pvppcoe.ac.in>
2021-06-19 21:36:16 +0530

    added material-ui

--- from/src/App.js
+++ from/src/App.js
@@ -1,3 +1,4 @@
+import { TextField } from '@material-ui/core';
 import './App.css';
 
 function App() {
@@ -5,36 +6,14 @@ function App() {
     <div className="App">
       <h1>About Form</h1>
       <form>
-        <div>
-          <label>Name: </label>
-          <input type="text" placeholder="xyz" required />
-        </div>
-        <div>
-          <label>Goals: </label>
-          <input type="text" placeholder="Short term goals..." required />
-        </div>
-        <div>
-          <label>Habits: </label>
-          <input type="text" placeholder="Reading, Writing..." required />
-        </div>
-        <div>
-          <label>Age: </label>
-          <input type="number" placeholder="1200" required />
-        </div>
-        <div>
-          <label htmlFor="">Interests: </label>
-          <input type="text" placeholder="Researching" required />
-        </div>
-        <div>
-          <label htmlFor="">Experience: </label>
-          <input type="text" placeholder="Experiences" required />
-        </div>
-        <div>
-          <label htmlFor="">Volunteers: </label>
-          <input type="text" placeholder="Volunteering" required />
-        </div>
+        <TextField id="filled-basic" label="Name" variant="filled" required />
+        <TextField id="filled-basic" label="Goals" variant="filled" required />
+        <TextField id="filled-basic" label="Habits" variant="filled" required />
+        <TextField id="filled-basic" label="Age" variant="filled" required />
+        <TextField id="filled-basic" label="Interests" variant="filled" required />
+        <TextField id="filled-basic" label="Experience" variant="filled" required />
+        <TextField id="filled-basic" label="Volunteering" variant="filled" required />
       </form>
-      <hr />
     </div>
   );
 }
