d20e29a0fe0f85980380271ea4b974f945829921
aditi <aditi2915sharma@gmail.com>
2021-07-02 12:02:09 +0530

    Registration Form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from "react";
-import {add,sub, mul, div}  from './Calc'
+
+import './App.css';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, TextField, Toolbar, Container } from '@material-ui/core'
+import { Dashboard, TextFields } from '@material-ui/icons';
+import { InputLabel } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles' ;
 
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-    <ul>
-      <li>The sum of two numbers is {add(40,4)}</li>
-      <li>The diff of two numbers is {sub(40,4)}</li>
-      <li>The multiplication of two numbers is {mul(40,4)}</li>
-      <li>The division of two numbers is {div(40,4)}</li>
-    </ul>
+    <CssBaseline />
+    
+    <AppBar position= "relative" gutterBottom>
+      <Toolbar>
+<Dashboard />
+<Typography variant= "h6">
+  Registration Form
+</Typography>
+</Toolbar>
+    </AppBar>
+
+    <Container style= {{marginTop: '100px'}} >
+      <Card>
+        <CardContent>
+          <Typography variant= "h6" gutterBottom>
+            Name
+          </Typography>
+          <TextField required label="Full Name"  />
+
+          <Typography variant= "h6" gutterBottom style= {{marginTop: '20px'}}>
+            E-mail
+          </Typography>
+          <TextField required label="Email Id"  />
+
+          <Typography variant= "h6" gutterBottom style= {{marginTop: '20px'}}>
+            Phone Number
+          </Typography>
+          <TextField required label="Phone Number"  />
+          <Button variant="contained" color= "primary"  style={{marginTop: '120px'}} >
+    Submit
+  </Button>
+
+        </CardContent>
+      </Card>
+    </Container>
+
+
+
     </>
-  )
+  );
 }
+
 export default App;
